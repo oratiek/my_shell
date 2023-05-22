b8a7c429224c7f6633854487629c288ecd027917
oratiek <oratiek522@gmail.com>
2023-05-22 13:17:38 +0900

    second commit

---
+++ cat.c
@@ -0,0 +1,45 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+
+void print(char* argv) {
+    int i = 0;
+    char buf;
+    while (1) {
+        buf = argv[i];
+        if (buf == '\0') {
+            write(1, "\n", 1);
+            break;
+        } else {
+            write(1, &buf, 1);
+        }
+        i++;
+    }
+}
+
+// get file and show the content
+
+int main(int argc, char *argv[]) {
+    int fd;
+    size_t buff_size = 256;
+    char buff[buff_size];
+    int res;
+
+    if (argc != 2) {
+        print("not enough argument..");
+        return -1;
+    }
+
+    fd = open(argv[1], O_RDONLY);
+    if (fd < 0) {
+        print("no such a file..");
+    }
+    while (read(fd, buff, buff_size) != 0) {
+        print(buff);
+    }
+
+    
+    return 0;
+}
