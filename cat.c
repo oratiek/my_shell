0d2351562ab0eca645d50b6d18406c9fe02aa760
oratiek <oratiek522@gmail.com>
2023-05-22 13:21:57 +0900

    modified

--- cat.c
+++ cat.c
@@ -35,6 +35,7 @@ int main(int argc, char *argv[]) {
     fd = open(argv[1], O_RDONLY);
     if (fd < 0) {
         print("no such a file..");
+        return -1;
     }
     while (read(fd, buff, buff_size) != 0) {
         print(buff);
